5446e5936f4a4dccb0ded841d35a0bf061577a7d
thatiparthimuralipremchand <62003371+thatiparthimuralipremchand@users.noreply.github.com>
2020-07-10 12:48:50 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+//shw cart
+(function() {
+    const cartInfo = document.getElementById("cart-Info");
+    const cart = document.getElementById("cart");
+
+    cartInfo.addEventListener("click",function() {
+    cart.classList.toggle("show-cart");   
+    });
+})();
+// add items to the cart
+(function(){
+    const cartBtn = document.querySelectorAll('.store-item-icon');
+
+})();
